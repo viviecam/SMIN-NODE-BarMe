d423527144d2e6862721fa953111734483574973
viviecam <camille.vivier@hotmail.fr>
2018-03-12 01:09:02 +0100

    Chat socket io - working + Add Boostrap 4 styling

--- public/js/client.js
+++ public/js/client.js
@@ -1,18 +1,63 @@
-(function($){
-    console.log('ouiiiiiiiiiiiiii!!!!!!!')
-    // On se connecte, depuis le site, à socket io
+$(document).ready(function () {
+    // On se connecte, depuis notre site, à socket io
     var socket = io.connect('http://localhost:3124');
 
+    // Quand l'utilisateur soumet le formulaire de login
     $('#loginform').submit(function(event){
         event.preventDefault();
-        // On créé un évement sur notre socket courante, 
-        // On va pouvoir envoyer cet évènement à notre serveur,
-        // Avec les paramètres de notre choix
+        // On génère un évenement "login" et on passe en paramètre,
+        // les infos de l'user qui vient de se connecter
         socket.emit('login', {
             username : $('#username').val(),
             email : $('#email').val()
         });
-    })
+    });
 
+    // Quand l'utilisateur est connecté, on fait disparaitre le formulaire de login
+    socket.on('logged', function(){
+        $('.login-overlay').fadeOut();
+        $('#messageinput').focus();
+    });
 
-})(jQuery);
+    /** GESTION DES USERS CONNECTES **/
+
+    // Quand l'évenement "newuser" a lieu
+    socket.on('newuser', function(user){
+        //alert('Voila il y a un nouvel utilisateur!');
+        $('#users').append('<li id = "'+ user.id +'">' + user.username + ' : ' + user.email + '</li>');
+    });
+
+    // Quand un utilisateur se déconnecte
+    socket.on('disconnectuser', function(user) {
+        $('#' + user.id).remove();
+    });
+
+    /** ENVOI DE MESSAGES **/
+    //On stocke le template fait en html, en js
+    var msgtpl = $('#msgtpl').html();
+    // Puis on le supprime de l'html après l'avoir récupéré en js
+    $('#msgtpl').remove();
+
+    // Pour récupérer l'id de l'user qui a envoyé le dernier msg
+    var lastmsg = false;
+
+    // Quand l'utilisateur soumet un nouveau message
+    $('#msgform').submit(function(event){
+        event.preventDefault();
+        console.log($('#messageinput').val());
+        socket.emit('newmsgarrived', {message: $('#messageinput').val()});
+        $('#messageinput').val('');
+        $('#messageinput').focus();
+    });
+
+    // Quand un nouveau message arrive et doit être affiché
+    socket.on('newmsgtodisplay', function(message) {
+        if (lastmsg != message.user.id) {
+            $('#msglist').append('<hr>');
+            lastmsg = message.user.id;
+        }
+        $('#msglist').append('<li>' + Mustache.render(msgtpl, message) + '</li>');
+        $('#msglist').animate({scrollTop : $('#msglist').prop('scrollHeight')}, 50);
+    });
+
+});
--- server.js
+++ server.js
@@ -1,13 +1,22 @@
+// TUTO VIDEO 
 // var http = require('http');
 // var url = require('url');
 
 // serverBarNme = http.createServer(function(req,res) {
     
 // });
+// FIN TUTO VIDEO
+
 var ejs = require('ejs');
 var express = require('express');
 var app = express();
 var serverBarMe = require('http').Server(app);
+
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+// On déclare où est notre répertoire static
+app.use('/public', express.static('public'));
 
 // Gestion des urls et des template ejs
 app.get('/login', function(req, res) {
@@ -19,23 +28,88 @@ app.use(function(req, res){
     res.status(404).send('Not Found');
 });
 
-// On déclare où est notre répertoire static
-app.use(express.static('public'));
-// app.use(express.static(path.join(__dirname, '/public')));
-// app.use(express.static(__dirname + '/public'));
-// app.use('/public', express.static(path.join(__dirname, 'public')))
 
 // On déclare que le serveur écoute sur le port 3124
 serverBarMe.listen(3124);
 
-// On lie socket au server
-var io = require('socket.io').listen(serverBarMe);
 
-// Quand il y a une connexion sur le serveur, sur l'une des sockets 
+
+// On lie socket au server, on lui dit d'ecouter les connexions au niveau de notre serveur
+var io = require('socket.io').listen(serverBarMe);
+// Variable pour stocker les utilisateurs connectés
+var users = {};
+// Variable pour stocker les messages
+var messages = [];
+var history = 2;
+
+// Quand il y a une connexion sur l'une des sockets = Quand un utilisateur ouvre la page
 io.sockets.on('connection', function (socket) {
-    // On réceptionne l'évènement
+    // Fonction qui prend en paramètre, la socket de l'utilisateur en cours
+    console.log('Un nouvel utilisateur à ouvert la page');
+
+    // Pour tous les utilisateurs déjà connectés et existant dans users
+    // On emet l'évenement "newuser", qui va généré leur ajout visuellement dans la liste, coté client
+    for (var k in users) {
+        socket.emit('newuser', users[k]);
+    }
+
+    // Pour tous les messages déjà existants
+    for (var k in messages) {
+        socket.emit('newmsgtodisplay', messages[k]);
+    }
+
+    // On créer une variable currentUser pour pouvoir utiliser l'user ailleurs
+    var currentUser = false;
+
+    // Quand un utilisateur se connecte
     socket.on('login', function(user){
+        // On récupère l'user qui vient de se connecter
         console.log(user);
+        currentUser = user;
+        currentUser.id = user.email;
+
+        // On emet un évenement pour dire que l'utilisateur est bien connecté
+        socket.emit('logged');
+
+        // On rajoute l'user courrant dans la liste des users
+        users[currentUser.id] = currentUser;
+        // Broadcast permet de "prevenir" tous les socket connectés (tous les users) que l'évenement à eu lieu,
+        // Sauf la socket courante :
+        //socket.broadcast.emit('newuser');
+        // Pour prévenir tout le monde, y compris la socket courante
+        // io.sockets, car il recence tout les users actuellement connectés
+        io.sockets.emit('newuser', currentUser);
+    });
+
+    // Quand l'utilisateur se déconnecte / quitte la page
+    socket.on('disconnect', function(){
+        // Pour être sur qu'un user ne puisse pas se deconnecter sans s'être préalablement connecté
+        // Si je n'ai pas encore d'user dans mon currentUser (car l'user ne s'est pas encore connecté)
+        if (!currentUser) {
+            return false;
+        }
+        // On le supprime de la liste des users
+        delete users[currentUser.id];
+        // Et on emet un évenement pour prevenir tout le monde qu'un user s'est déconnecté
+        io.sockets.emit('disconnectuser', currentUser);
+    });
+
+    // Quand un message arrive
+    socket.on('newmsgarrived', function(message){
+        message.user = currentUser;
+        date = new Date();
+        message.hour = date.getHours();
+        message.min = date.getMinutes();
+        message.day = date.getDate();
+        message.month = date.getMonth() + 1;
+        message.year = date.getFullYear();
+        // On verifie que le tableau de message ne soit pas plein pour ajouter le nouveau message au tableau
+        if (messages.length > history) {
+            // Shift permet de supprimer le premier element du tableau
+            messages.shift();
+        }
+        messages.push(message);
+        io.sockets.emit('newmsgtodisplay', message);
     })
 
 })
